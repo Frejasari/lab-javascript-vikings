59c45201b212e69aa197f3ccc1fdb90959512431
Sari <freja.sender@gmail.com>
2018-05-25 15:23:50 +0200

    add soldier

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,16 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+  this.health = healthArg;
+  this.strength = strengthArg;
+}
+
+Soldier.prototype.attack = function() {
+  return this.strength;
+};
+
+Soldier.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+};
 
 // Viking
 function Viking() {}
