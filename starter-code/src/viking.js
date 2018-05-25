2e4e1b1d2526d325fb61ed5a6520a093c42803b4
Sari <freja.sender@gmail.com>
2018-05-25 15:24:22 +0200

    add saxon

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -32,7 +32,18 @@ Viking.prototype.battleCry = function() {
 };
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength);
+}
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+  if (this.health > 0) {
+    return "A Saxon has received " + damage + " points of damage";
+  } else {
+    return "A Saxon has died in combat";
+  }
+};
 
 // War
 function War() {}
